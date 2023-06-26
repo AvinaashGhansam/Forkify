0c2e7383eacad0e463f453f382e5345a64827e89
Avinaash Ghansam <avinaash.ghansam.1@gmail.com>
2023-06-26 19:50:55 -0400

    refactored in progress to implemement mvc architecture

---
+++ src/js/model.js
@@ -0,0 +1,38 @@
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+
+    // convert to JSON
+    const data = await res.json();
+    console.log(data);
+
+    // check response status
+    if (!res.ok) {
+      throw new Error(`${data.message} (${res.status}) `);
+    }
+
+    // Reformat Data
+    let { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      serving: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+  {
+  }
+};
